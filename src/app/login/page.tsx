b546e4ae21ab94ccf5b5585857ee9645f1b491ed
suyog prasai <suyog224356@gmail.com>
2025-08-28 03:44:57 +0000

    improved the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,29 +1,10 @@
-import { GalleryVerticalEnd } from "lucide-react"
-
 import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2 bg-white">
-      <div className="flex flex-1 flex-col items-center justify-center space-y-8">
-        {/* Netflix Logo */}
-        <div className="text-center">
-          <h1 className="text-red-600 text-5xl tracking-widest font-bebas-neue">
-            BAKHABAZAR
-          </h1>
-        </div>
-
-        {/* Login Form */}
-        <div className="w-full max-w-xs">
-          <LoginForm />
-        </div>
-      </div>
-      <div className="bg-muted relative hidden lg:block">
-        <img
-          src="/login_featured.png"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+    <div className="bg-sidebar flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
       </div>
     </div>
   )
